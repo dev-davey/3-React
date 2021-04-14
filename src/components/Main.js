1d73ad5e66bc9f22ba046432e1d72d0747ef89e1
davey <davey89451@gmail.com>
2021-04-14 12:09:16 -0700

    adding to single page app

---
+++ src/components/Main.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Directory from "./Directory";
+import CampsiteInfo from "./CampsiteInfo";
+import Home from "./Home";
+import Contact from "./Contact";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { CAMPSITES } from "../shared/campsites";
+import { COMMENTS } from "../shared/comments";
+import { PARTNERS } from "../shared/partners";
+import { PROMOTIONS } from "../shared/promotions";
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES,
+      comments: COMMENTS,
+      partners: PARTNERS,
+      promotions: PROMOTIONS,
+    };
+  }
+
+  render() {
+    const HomePage = () => {
+      return (
+        <Home
+          campsite={
+            this.state.campsites.filter((campsite) => campsite.featured)[0]
+          }
+          promotion={
+            this.state.promotions.filter((promotion) => promotion.featured)[0]
+          }
+          partner={this.state.partners.filter((partner) => partner.featured)[0]}
+        />
+      );
+    };
+
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route
+            exact
+            path="/directory"
+            render={() => <Directory campsites={this.state.campsites} />}
+          />
+          <Route exact path="/contactus" component={Contact} />
+          <Redirect to="/home" />
+        </Switch>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Main;
