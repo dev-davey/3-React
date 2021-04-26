dc486fb225ed54b3b6d3198d6b2cf70de88ee60e
davey <davey89451@gmail.com>
2021-04-26 10:51:58 -0700

    Combining Reducers

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import reactDom from 'react-dom';
+
 
 function RenderPartner({partner}){
     if (partner) {
