8f3016088e86cb10d90f7e44e418273304ea6085
davey <davey89451@gmail.com>
2021-04-28 13:47:58 -0700

    fetch post comment

--- src/components/CampsiteInfo.js
+++ src/components/CampsiteInfo.js
@@ -33,7 +33,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+    this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
   }
 
   toggleModal() {
@@ -120,7 +120,7 @@ function RenderCampsite({ campsite }) {
   );
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
   console.log(comments);
   if (comments) {
     return (
@@ -141,7 +141,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
             </div>
           );
         })}
-        <CommentForm campsiteId={campsiteId} addComment={addComment} />
+        <CommentForm campsiteId={campsiteId} postComment={postComment} />
       </div>
     );
   }
@@ -189,7 +189,7 @@ function CampsiteInfo(props) {
           <RenderCampsite campsite={props.campsite} />
           <RenderComments 
           comments={props.comments}
-          addComment={props.addComment}
+          postComment={props.postComment}
           campsiteId={props.campsite.id}
           />
         </div>
